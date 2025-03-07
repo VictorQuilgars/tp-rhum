ddc300a70d5ff6b488487886003b0aa0773f81fb
VictorQuilgars <vquilgars@icloud.com>
2025-03-07 11:08:52 +0100

    refactor(authController): add indent

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,16 +3,18 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
 
 const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" });
+  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 };
 
 exports.register = async (req, res) => {
   try {
-
     console.log(req.body);
     const { nom, email, password, adresse } = req.body;
     const userExists = await User.findOne({ email });
-    if (userExists) return res.status(400).json({ message: "Email already in use" });
+    if (userExists)
+      return res.status(400).json({ message: "Email already in use" });
 
     const user = new User({ nom, email, password, adresse });
     await user.save();
@@ -27,10 +29,12 @@ exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "Wrong email or password" });
+    if (!user)
+      return res.status(400).json({ message: "Wrong email or password" });
 
     const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(400).json({ message: "Wrong email or password" });
+    if (!isMatch)
+      return res.status(400).json({ message: "Wrong email or password" });
 
     res.json({ token: generateToken(user), userId: user._id });
   } catch (error) {
