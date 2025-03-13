25f2276032e4fb30867d85400f14eea22036e540
VictorQuilgars <vquilgars@icloud.com>
2025-03-13 11:16:56 +0100

    feat(ingredients): add search ingredients

--- src/controllers/ingredientController.js
+++ src/controllers/ingredientController.js
@@ -1,4 +1,4 @@
-const Ingredient = require("../models/Ingredients");
+const Ingredient = require("../models/vqu-ingredients");
 
 exports.addIngredient = async (req, res) => {
   try {
@@ -11,6 +11,14 @@ exports.addIngredient = async (req, res) => {
     const validTypes = ["fruits", "épices", "sucres", "autres"];
     if (!validTypes.includes(type)) {
       return res.status(400).json({ message: "Type d'ingrédient invalide." });
+    }
+
+    if (await Ingredient.findOne({ nom })) {
+      return res.status(400).json({ message: "Cet ingrédient existe déjà." });
+    }
+
+    if (prix <= 0) {
+      return res.status(400).json({ message: "Le prix doit être supérieur à 0." });
     }
 
     const newIngredient = new Ingredient({
@@ -31,3 +39,43 @@ exports.addIngredient = async (req, res) => {
     res.status(500).json({ message: "Erreur serveur", error });
   }
 };
+
+exports.searchIngredients = async (req, res) => {
+    try {
+        const { nom, type } = req.query;
+
+        let filter = {};
+
+        if (nom) {
+        filter.nom = { $regex: nom, $options: "i" }; // Recherche insensible à la casse
+        }
+
+        if (type) {
+        filter.type = type;
+        }
+
+        const ingredients = await Ingredient.find(filter);
+
+        res.status(200).json({
+        message: "Résultats de la recherche",
+        total: ingredients.length,
+        ingredients,
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Erreur serveur", error });
+    }
+};
+
+exports.getAllIngredient = async (req, res) => 
+{
+  try {
+    const ingredients = await Ingredient.find();
+    res.status(200).json({
+      message: "Liste de tous les ingrédients",
+      total: ingredients.length,
+      ingredients,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Erreur serveur", error });
+  }
+}
--- src/routes/ingredientRoutes.js
+++ src/routes/ingredientRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { addIngredient } = require("../controllers/ingredientController");
+const { addIngredient, searchIngredients, getAllIngredient,  } = require("../controllers/ingredientController");
 const protect = require("../middlewares/authMiddleware"); // JWT pour sécuriser
 
 const router = express.Router();
 
 router.post("/", protect, addIngredient);
+router.get("/search", protect, searchIngredients);
+router.get("/all-ingredients", protect, getAllIngredient);
 
 module.exports = router;
