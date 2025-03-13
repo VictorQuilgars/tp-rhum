f0af2dced2ee3a7762c186625c237020d507d9e3
VictorQuilgars <vquilgars@icloud.com>
2025-03-13 12:30:50 +0100

    feat(rhums): add get all rhums

--- src/controllers/rhumControllers.js
+++ src/controllers/rhumControllers.js
@@ -2,6 +2,15 @@ const Rhum = require('../models/rhums');
 
 exports.getAllRhums = async (req, res) => {
   try {
+    const rhums = await Rhum.find();
+    res.status(200).json(rhums);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getRhumsWithFilter = async (req, res) => {
+  try {
     const filters = req.query;
     const rhums = await Rhum.find(filters);
     res.status(200).json(rhums);
