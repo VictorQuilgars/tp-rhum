53796efd732d43218f8fa63b621c29e1d19177f8
VictorQuilgars <vquilgars@icloud.com>
2025-05-16 15:20:37 +0200

    add doc

---
+++ src/config/swagger.js
@@ -0,0 +1,37 @@
+const swaggerJSDoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "API Rhum Arrangé",
+      version: "1.0.0",
+      description: "Documentation de l'API pour la gestion des recettes de rhum arrangé",
+    },
+    servers: [
+      {
+        url: "http://localhost:8000",
+        description: "Serveur local",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        cookieAuth: {
+          type: "apiKey",
+          in: "cookie",
+          name: "token", // ⚠️ le nom de ton cookie JWT
+        },
+      },
+    },
+    security: [
+      {
+        cookieAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"], // où sont les commentaires Swagger dans les routes
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+module.exports = swaggerSpec;
